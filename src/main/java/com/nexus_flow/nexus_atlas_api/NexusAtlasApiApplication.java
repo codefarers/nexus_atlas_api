033850b442ea3187754fc0fc9de19538bd1f6b56
romanusbett <bettromanus@gmail.com>
2026-05-02 22:10:29 +0300

    initial commit

---
+++ src/main/java/com/nexus_flow/nexus_atlas_api/NexusAtlasApiApplication.java
@@ -0,0 +1,13 @@
+package com.nexus_flow.nexus_atlas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAtlasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusAtlasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus_flow/nexus_atlas_api/NexusAtlasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus_flow.nexus_atlas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusAtlasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
